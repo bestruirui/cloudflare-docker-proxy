df1e9fe11b6c0bef5c007a87d6f71e34bcfff1bb
BESTRUI <bestrui@stu.ncst.edu.cn>
2024-07-19 09:48:18 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.bestrui.top": dockerHub,
+  "quay.bestrui.top": "https://quay.io",
+  "gcr.bestrui.top": "https://gcr.io",
+  "k8s-gcr.bestrui.top": "https://k8s.gcr.io",
+  "k8s.bestrui.top": "https://registry.k8s.io",
+  "ghcr.bestrui.top": "https://ghcr.io",
+  "cloudsmith.bestrui.top": "https://docker.cloudsmith.io",
+  "ecr.bestrui.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.bestrui.top": dockerHub,
 };
 
 function routeByHosts(host) {
